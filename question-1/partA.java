25f345f84d836e8262bafe5bce77458035f8f823
JeffreyLin42 <xingyaochangkong@gmail.com>
2026-05-20 10:00:06 -0400

    Add username creation logic in Account constructor
    Implement username creation logic in Account class.

--- question-1/partA.java
+++ question-1/partA.java
@@ -1,2 +1,27 @@
-public class Account{
+private String username; 
+/**
+* Creates a username based on the parameter requestedName. If the
+* username is unavailable, appends successive integers, beginning
+* with 1, to requestedName until an available username is found,
+* as described in part (a).
+*/
+public Account(String requestedName){
+  int i=1;
+  String temp = requestedName;
+  while(!isAvailable(requestedName)){
+    username =temp+i;
+    i++;
+  }
   
+}
+
+
+
+  
+/**
+* Returns true if the parameter str is an available username;
+* returns false otherwise.
+*/
+public static boolean isAvailable(String str)
+{ /* implementation not shown */ }
+/**
