038561a2a5eab0287186e110d8edb4f065beaba3
JeffreyLin42 <xingyaochangkong@gmail.com>
2026-05-23 18:33:33 -0400

    Add getPointsForRow method to GameBoard class
    Implement getPointsForRow method to calculate points based on row color consistency.

--- question-4/GameBoard.java
+++ question-4/GameBoard.java
@@ -1,3 +1,16 @@
+private Space[][] board;
+
 public int getPointsForRow(int targetRow){
-  
+  int sum;
+  Boolean isSame=true;
+  for(int c=0; c<board[targetRow].length;c++){
+    if(!board[targetRow][c].getColor().equals(board[targetRow][0].getColor())){
+      isSame=false;
+    }
+    sum+=board[targetRow][c].getPionts();
+  }
+  if(isSame){
+    return sum*2;
+  }
+  return sum;
 }
