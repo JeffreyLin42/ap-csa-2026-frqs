a57c72cdedea3ed80c707c7a1014d8a1331e45b0
JeffreyLin42 <xingyaochangkong@gmail.com>
2026-05-23 16:53:11 -0400

    Correct absence comparison logic in moreHistoryThanMathAbsences
    Fix logic in moreHistoryThanMathAbsences method to correctly compare absences.

--- question-3/CourseRecored.java
+++ question-3/CourseRecored.java
@@ -1,3 +1,21 @@
+/* The students enrolled in a history course */
+private ArrayList<CourseRecord> historyList;
+/* The students enrolled in a math course */
+private ArrayList<CourseRecord> mathList;
+
+
 public int moreHistoryThanMathAbsences(){
-  
+  int count=0;
+  for(int i=0; i < historyList.size(); i++){
+    for(int k=0; k< mathList.size();k++){
+      String hisName = historyList.get(i).getStudentID();
+      String mathName = mathList.get(k).getStudentID();
+      int hisA = historyList.get(i).getAbsences();
+      int mathA = mathList.get(i).getAbsences();
+      if(hisName.equals(mathName)&& hisA>mathA){
+        count++;
+      }
+    }
+  }
+  return count;
 }
